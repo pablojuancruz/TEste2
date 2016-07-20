acc8ecb7069f83fcc7ee4f26f195f8a92be98271
pablo <pablo@DESKTOP-CSGEF6B>
2016-07-19 23:39:34 -0300

    Teste outro tipo de solucao

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1
 {
     class Program
-    {
+    {/// <summary>
+    /// 
+    /// Teste
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
         }
